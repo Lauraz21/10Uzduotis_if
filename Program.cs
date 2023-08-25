6dc3cbde1c1f6fc91d9f209d20cbc0b5bafd05bc
Laura <Laura@DESKTOP-BESSB6E>
2023-08-25 19:44:33 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Iveskite 3 skirtingus skaicius: ");
+float number = Convert.ToSingle(Console.ReadLine());
+float number2 = Convert.ToSingle(Console.ReadLine());
+float number3 = Convert.ToSingle(Console.ReadLine());
+
+if ((number + number2) >= number3 &&
+   (number + number3) >= number2 &&
+   (number2 + number3) >= number)
+{
+    Console.WriteLine("Galima sudaryti trikampį");
+}
+else
+{
+    Console.WriteLine("Negalima sudaryti trikampio");
+}
